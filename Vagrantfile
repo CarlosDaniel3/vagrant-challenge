213cbbc659265fc0ca96cdfc0c07816b7a2d934c
CarlosDaniel3 <carloscggames@gmail.com>
2022-10-14 08:57:14 -0300

    Provisioning - Shell (External Script)

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,7 @@
 Vagrant.configure("2") do |config|
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = "ubuntu_vm"
+  end
   config.vm.box = "hashicorp/bionic64"
+  config.vm.provision "shell", path: "script.sh"
 end
---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update
+apt install -y vim
+apt install -y curl
+apt install telnet
+apt install unzip
+apt install wget
+apt install net-tools
+apt install htop
+apt install -y nmap
+
+apt install -y nginx
+
+cd /home/
+useradd test2
+
